e472a71d01288454a75c4f9740dc283313c4f2be
starjunk <zarrelligiovannj@gmail.com>
2024-05-07 15:59:17 +0200

    Initial Commit

---
+++ Events2024/Events2024App.swift
@@ -0,0 +1,17 @@
+//
+//  Events2024App.swift
+//  Events2024
+//
+//  Created by iedstudent on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Events2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
