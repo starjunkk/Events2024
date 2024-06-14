70206eac9d37961f8e7fedbb13d69334bc4866d6
Giovanni Maria Zarrelli <148645923+starjunkk@users.noreply.github.com>
2024-06-14 18:39:36 +0200

    lezione finale

--- Cesk Events/EventsView.swift
+++ Cesk Events/EventsView.swift
@@ -1,10 +1,3 @@
-//
-//  EventsView.swift
-//  Cesk Events
-//
-//  Created by iedstudent on 07/05/24.
-//
-
 import SwiftUI
 import DBNetworking
 
@@ -14,81 +7,81 @@ struct EventsView: View {
     @State var eventsToShow: [Event] = []
     @State var selectedEvent: Event?
     
-    //visualizzazione di defaul: Lista
+    // visualizzazione di default: Lista
     @State var isList = true
     
     // MARK: - VIEW
-      var body: some View {
-          NavigationView {
-              ScrollView {
-                  LazyVGrid(columns: [
-                      GridItem(.adaptive(minimum: isList ? 300 : 150))
-                  ], spacing: 0) {
-                      ForEach(self.eventsToShow) { event in
-                          
-                          // UI di ogni elemento
-                          NavigationLink(destination: EventDetailView(eventToShow: event)) {
-                              VStack {
-                                  ZStack {
-                                      NetworkImage(url: event.coverUrl)
-                                          .frame(height: 200)
-                                          .clipShape(Rectangle())
-                                          .padding(10)
-                                      
-                                      
-                                      Rectangle()
-                                          .foregroundStyle(.clear)
-                                          .background(
-                                              LinearGradient(
-                                                  colors: [Color.ui.buttons.opacity(0.1), Color.clear],
-                                                  startPoint: .bottom,
-                                                  endPoint: .top
-                                              )
-                                          )
-                                  }
-                                  Text(event.name ?? "")
-                                  Text(event.date ?? "")
-                                  
-                                
-                                  Text(event.priceInEuro)
-                                      .font(.subheadline)
-                                      .foregroundColor(.gray)
-                                      .padding(.top, 5)
-                              }
-                              .contentShape(Rectangle())
-                          }
-                          .buttonStyle(PlainButtonStyle())
-                          .onTapGesture {
-                              selectedEvent = event
-                          }
-                      }
-                  }
-                  .navigationTitle("Homepage")
-                  .navigationBarTitleDisplayMode(.inline)
-                  .onAppear {
-                      Task {
-                          await fetchEvents()
-
-                      }
-                  }
-              }
-              .toolbar {
-                  ToolbarItem(placement: .topBarTrailing) {
-                      Button {
-                          withAnimation { self.isList.toggle() }
-                          // Switch tra lista e griglia
-                      } label: {
-                          Image(systemName: isList ? "square.grid.2x2" : "rectangle.grid.1x2")
-                      }
-                  }
-              }
-              // Barra fissa in basso
-              
-          }
-      }
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: [
+                    GridItem(.adaptive(minimum: isList ? 300 : 150))
+                ], spacing: 12) {
+                    ForEach(self.eventsToShow) { event in
+                        
+                        // UI di ogni elemento
+                        NavigationLink(destination: EventDetailView(eventToShow: event)) {
+                            VStack {
+                                ZStack(alignment: .topTrailing) {
+                                    NetworkImage(url: event.coverUrl)
+                                        .frame(height: 310)
+                                        .cornerRadius(16)
+                                        .clipShape(Rectangle())
+                                        .padding(.leading, isList ? 16 : 8)
+                                        .padding(.trailing, isList ? 16 : 8)
+                                        .padding(.top, 6)
+                                        .padding(.bottom, 4)
+                                    
+                                    VStack {
+                                        Text(StringHelper.formatDate(event.date, in: "dd"))
+                                            .font(.system(size: 18, weight: .bold))
+                                        Text(StringHelper.formatDate(event.date, in: "MMM").uppercased())
+                                            .font(.system(size: 18, weight: .bold))
+                                    }
+                                    .padding(16)
+                                    .background(Color.black.opacity(0.7))
+                                    .cornerRadius(12)
+                                    .foregroundColor(.white)
+                                    .padding([.top, .trailing], 20)
+                                    .offset(x: -10) // Sposta la VStack leggermente a sinistra
+                                }
+                                Text(event.name ?? "")
+                                    .bold()
+                                Text(event.priceInEuro)
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                            }
+                            .contentShape(Rectangle())
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                        .onTapGesture {
+                            selectedEvent = event
+                        }
+                    }
+                }
+                .navigationTitle("Homepage")
+                .navigationBarTitleDisplayMode(.inline)
+                .onAppear {
+                    Task {
+                        await fetchEvents()
+                    }
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        withAnimation { self.isList.toggle() }
+                        // Switch tra lista e griglia
+                    } label: {
+                        Image(systemName: isList ? "square.grid.2x2" : "rectangle.grid.1x2")
+                    }
+                }
+            }
+        }
+    }
     
     //MARK: - FUNZIONI
-    func fetchEvents() async{
+    func fetchEvents() async {
         
         ///1 Preparo la richiesta
         let request = DBNetworking.request(url: "https://edu.davidebalistreri.it/app/v3/events")
